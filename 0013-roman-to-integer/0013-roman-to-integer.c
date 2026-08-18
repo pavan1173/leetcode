53dc7c5df0daad140aa8767052fae28776bb884b
M Pavan Kumar <mpavankumar110405@gmail.com>
2026-08-18 23:47:32 +0530

    Time: 0 ms (100%), Space: 10.6 MB (100%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,29 @@
+
+int valur(char ch){
+    switch(ch){
+        case 'I' : return 1;
+        case 'V' : return 5;
+        case 'X' : return 10;
+        case 'L' : return 50;
+        case 'C' : return 100;
+        case 'D' : return 500;
+        case 'M' : return 1000;
+        default : return 0;
+    }
+}
+    int romanToInt(char* s){
+     int tot =0;
+     for(int i=0;s[i]!='\0';i++){
+        int curr= valur(s[i]);
+        int next = valur(s[i+1]);
+        if(curr<next){
+            tot -=curr;
+        }else{
+            tot+=curr;
+        }
+       
+
+     }
+ return tot;
+    
+}
